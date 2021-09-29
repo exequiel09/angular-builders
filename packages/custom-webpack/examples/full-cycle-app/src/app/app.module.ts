dba570ea7fe4a3805afb8042d02b71a7fbfc7034
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2021-09-29 17:54:31 +0800

    Add sample SSR app

--- packages/custom-webpack/examples/full-cycle-app/src/app/app.module.ts
+++ packages/custom-webpack/examples/full-cycle-app/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from '@app/app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
   providers: [],
   bootstrap: [AppComponent],
 })
